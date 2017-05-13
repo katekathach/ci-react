e936ca86a111793b18c2d1e28d7bcebe007e812c
kateka thach <katekathach@katekas-Mac-mini.local>
2017-05-12 21:21:26 -0400

    first

---
+++ .next/bundles/pages/resources.js
@@ -0,0 +1,73 @@
+
+          window.__NEXT_REGISTER_PAGE('/resources', function() {
+            var comp = module.exports =
+webpackJsonp([5],{
+
+/***/ 718:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(__resourceQuery) {
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = __webpack_require__(18);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/katekathach/sites/ci-react/pages/resources.js?entry";
+
+exports.default = function () {
+  return _react2.default.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    }
+  }, "Resources");
+};
+
+ ;(function register() { /* react-hot-loader/webpack */ if (true) { if (typeof __REACT_HOT_LOADER__ === 'undefined') { return; } if (typeof module.exports === 'function') { __REACT_HOT_LOADER__.register(module.exports, 'module.exports', "/Users/katekathach/sites/ci-react/pages/resources.js"); return; } for (var key in module.exports) { if (!Object.prototype.hasOwnProperty.call(module.exports, key)) { continue; } var namedExport = void 0; try { namedExport = module.exports[key]; } catch (err) { continue; } __REACT_HOT_LOADER__.register(namedExport, key, "/Users/katekathach/sites/ci-react/pages/resources.js"); } } })();
+    (function (Component, route) {
+      if (false) return
+      if (false) return
+
+      var qs = __webpack_require__(110)
+      var params = qs.parse(__resourceQuery.slice(1))
+      if (params.entry == null) return
+
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(module.exports.default || module.exports, "/resources")
+  
+/* WEBPACK VAR INJECTION */}.call(exports, "?entry"))
+
+/***/ }),
+
+/***/ 719:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(718);
+
+
+/***/ })
+
+},[719]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9wYWdlcy9yZXNvdXJjZXMuanM/ZGRlMGIwMSJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7O0FBQUE7Ozs7Ozs7O2tCQUFlO3lCQUNiOztnQkFBQTtrQkFBQTtBQUFBO0FBQUE7QUFERixFIiwiZmlsZSI6ImJ1bmRsZXMvcGFnZXMvcmVzb3VyY2VzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgKCkgPT4gKFxuICA8ZGl2PlJlc291cmNlczwvZGl2PlxuKVxuXG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIC4vcGFnZXMvcmVzb3VyY2VzLmpzP2VudHJ5Il0sInNvdXJjZVJvb3QiOiIifQ==
+            return { page: comp.default }
+          })
+        
